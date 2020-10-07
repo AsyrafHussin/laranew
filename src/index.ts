033517b5e18650a3fb73c8f9d1c0a60a5e4ad002
Asyraf Hussin <asyrafhussin4@gmail.com>
2020-10-07 18:21:36 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { Command, flags } from "@oclif/command";
+import { cd, exec } from "shelljs";
+
+class Laranew extends Command {
+  static description =
+    "Generate Laravel project based on https://github.com/AsyrafHussin/laravel-boilerplate-v2";
+
+  static flags = {
+    version: flags.version({ char: "v" }),
+  };
+
+  static args = [{ name: "project" }];
+
+  async run() {
+    const { argv } = this.parse(Laranew);
+
+    const projectName = argv[0] ?? "project";
+
+    exec(
+      `git clone https://github.com/AsyrafHussin/laravel-boilerplate-v2.git ${projectName}`
+    );
+    cd(`${projectName}`);
+    exec("./setup.sh --color=always");
+  }
+}
+
+export = Laranew;
