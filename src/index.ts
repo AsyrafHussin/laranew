2fc11eaf9c02aadb320747ff08c9b43b41efc594
Asyraf Hussin <asyrafhussin4@gmail.com>
2020-10-29 06:41:36 +0800

    Add prettierrc config

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,26 @@
-import { Command, flags } from "@oclif/command";
-import { cd, exec } from "shelljs";
+import {Command, flags} from '@oclif/command'
+import {cd, exec} from 'shelljs'
 
 class Laranew extends Command {
   static description =
-    "Generate Laravel project based on https://github.com/AsyrafHussin/laravel-boilerplate-v2";
+    'Generate Laravel project based on https://github.com/AsyrafHussin/laravel-boilerplate-v2';
 
   static flags = {
-    version: flags.version({ char: "v" }),
+    version: flags.version({char: 'v'}),
   };
 
-  static args = [{ name: "project" }];
+  static args = [{name: 'project'}];
 
   async run() {
-    const { argv } = this.parse(Laranew);
+    const {argv} = this.parse(Laranew)
 
-    const projectName = argv[0] ?? "project";
+    const projectName = argv[0] ?? 'project'
 
     exec(
-      `git clone https://github.com/AsyrafHussin/laravel-boilerplate-v2.git ${projectName}`
-    );
-    cd(`${projectName}`);
-    exec("./setup.sh --color=always");
+      `git clone https://github.com/AsyrafHussin/laravel-boilerplate-v2.git ${projectName}`,
+    )
+    cd(`${projectName}`)
+    exec('./setup.sh --color=always')
   }
 }
 
