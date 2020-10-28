a971733f53ecb556e4ef07a952a22a47dbfaf1cd
Asyraf Hussin <asyrafhussin4@gmail.com>
2020-10-29 06:47:47 +0800

    Update eslintrc to use semicolon

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import {Command, flags} from '@oclif/command'
-import {cd, exec} from 'shelljs'
+import {Command, flags} from '@oclif/command';
+import {cd, exec} from 'shelljs';
 
 class Laranew extends Command {
   static description = 'Generate Laravel project based on https://github.com/AsyrafHussin/laravel-boilerplate-v2';
@@ -11,13 +11,13 @@ class Laranew extends Command {
   static args = [{name: 'project'}];
 
   async run() {
-    const {argv} = this.parse(Laranew)
+    const {argv} = this.parse(Laranew);
 
-    const projectName = argv[0] ?? 'project'
+    const projectName = argv[0] ?? 'project';
 
-    exec(`git clone https://github.com/AsyrafHussin/laravel-boilerplate-v2.git ${projectName}`)
-    cd(`${projectName}`)
-    exec('./setup.sh --color=always')
+    exec(`git clone https://github.com/AsyrafHussin/laravel-boilerplate-v2.git ${projectName}`);
+    cd(`${projectName}`);
+    exec('./setup.sh --color=always');
   }
 }
 
